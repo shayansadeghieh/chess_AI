5c2be592858954e906d0555e1ff81e28d9e6ad34
Shayan Sadeghieh <shayansadeghieh@shayans-mbp.home>
2020-08-03 21:29:27 +0100

    updates

--- state.py
+++ state.py
@@ -1,8 +1,33 @@
 import chess
+import numpy as np
 
 class State(object):
-    def __init__(self):
-        self.board = chess.Board()
+    def __init__(self, board=None):
+        if board is None:
+            self.board = chess.Board()
+        else:
+            self.board = board 
+    
+    def serialize(self):
+        assert self.board.is_valid()
+ 
+        bstate = np.zeros(64)
+
+        state = np.zeros((8,8,5))
+
+        for r in range(8):
+            for c in range(8):
+                state[r,c,0] = (bstate[r*8+c]/8)&1
+                state[r,c,1] = (bstate[r*8+c]/4)&1
+                state[r,c,2] = (bstate[r*8+c]/2)&1
+                state[r,c,3] = (bstate[r*8+c]/1)&1
+
+        
+
+        #Fourth column is who's turn it is
+        state[:, :, 4] = (self.board.turn*1.)
+
+        return state
     
     def edges(self):
         return self.board.legal_moves 
--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+from state import State
+import numpy as np 
 import os
 import chess.pgn 
 
@@ -5,11 +7,19 @@ for fn in os.listdir('data'):
     pgn = open(os.path.join('data', fn))
     while 1:
         try:
-            game = chess.pgn.read_game(pgn)
+            #Parse and extract each individual game from the pgn file, handles headers/ no headers
+            game = chess.pgn.read_game(pgn) 
         except Exception:
             break
+        print(game.headers['Result'])
+        #Developed value notation: 0 means draw, -1 means black won, 1 means white and * means ongoing game (who cares)
+        value = {'1/2-1/2':0, '0-1':-1, '1-0':1, '*': 'who_cares'}[game.headers["Result"]]
+        print(value)
         board = game.board()
-        for move in game.mainline_moves():
+        print(board)
+        #Iterate through all moves and play them on the board 
+        for i, move in enumerate(game.mainline_moves()):
             board.push(move)
-            print(board)
+            print(value, State(board).serialize())
         exit(0)
+    break
