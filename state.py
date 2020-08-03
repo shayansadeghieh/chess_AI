4444f75d4f4afdcda1be2d43446086e12bcaa159
Shayan Sadeghieh <shayansadeghieh@shayans-mbp.home>
2020-08-03 19:11:39 +0100

    Beginning chessAI for fun

---
+++ state.py
@@ -0,0 +1,17 @@
+import chess
+
+class State(object):
+    def __init__(self):
+        self.board = chess.Board()
+    
+    def edges(self):
+        return self.board.legal_moves 
+    
+    def value(self):
+        return 1
+
+if __name__ == "__main__":
+    s = State()
+    print(s.board)
+    print(s.edges())
+    
