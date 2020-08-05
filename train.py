93a64301f6e069b6c3622cd98b2c2a6b5b02f224
Shayan Sadeghieh <shayansadeghieh@shayans-mbp.home>
2020-08-05 08:56:23 +0100

    working training, a little bit buggy

--- train.py
+++ train.py
@@ -1,80 +1,103 @@
 import torch
 from torch.utils.data import Dataset
+import torch.nn.functional as F
 import torch.nn as nn
 import numpy as np
 
 class ChessValueDataset(Dataset):
     def __init__(self):
+        '''Init data and target outcomes'''
         dat = np.load("processed/dataset_1000.npz")  
         self.X = dat['arr_0']
         self.Y = dat['arr_1']
+        print("loaded", self.X.shape, self.Y.shape)
 
     def __len__(self):
+        '''Upperbound specified in this method'''
         return self.X.shape[0]
     
+    # def __getitem__(self, idx):
+    #     return {'X' : self.X[idx], 'Y' : self.Y[idx]}
     def __getitem__(self, idx):
-        return {'X' : self.X[idx], 'Y' : self.Y[idx]}
+        X = self.X[idx]
+        Y = self.Y[idx]
+        return X,Y
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.a1 = nn.Conv2d(5, 16, kernel_size = 3)
-        self.a2 = nn.Conv2d(16, 16, kernel_size = 3)
-        self.a3 = nn.Conv2d(16, 32, kernel_size = 3)
+        self.a1 = nn.Conv2d(5, 16, kernel_size = 3, padding = 1)
+        self.a2 = nn.Conv2d(16, 16, kernel_size = 3, padding = 1)
+        self.a3 = nn.Conv2d(16, 32, kernel_size = 3, stride = 2)
 
-        self.b1 = nn.Conv2d(32, 32, kernel_size = 3)
-        self.b2 = nn.Conv2d(32, 32, kernel_size = 3)
-        self.b3 = nn.Conv2d(32, 64, kernel_size = 3)
+        self.b1 = nn.Conv2d(32, 32, kernel_size = 2, padding = 1)
+        self.b2 = nn.Conv2d(32, 32, kernel_size = 2, padding = 1)
+        self.b3 = nn.Conv2d(32, 64, kernel_size = 2, stride = 2)
 
-        self.c1 = nn.Conv2d(64, 64, kernel_size = 3)
-        self.c2 = nn.Conv2d(64, 64, kernel_size = 3)
-        self.c3 = nn.Conv2d(64, 128, kernel_size = 3)
+        self.c1 = nn.Conv2d(64, 64, kernel_size = 2, padding = 1)
+        self.c2 = nn.Conv2d(64, 64, kernel_size = 2, padding = 1)
+        self.c3 = nn.Conv2d(64, 128, kernel_size = 2, stride = 2)
 
-        self.d1 = nn.Conv2d(128, 128, kernel_size = 3)
-        self.d2 = nn.Conv2d(128, 128, kernel_size = 3)
-        self.d3 = nn.Conv2d(128, 128, kernel_size = 3)
+        self.d1 = nn.Conv2d(128, 128, kernel_size = 1)
+        self.d2 = nn.Conv2d(128, 128, kernel_size = 1)
+        self.d3 = nn.Conv2d(128, 128, kernel_size = 1)
 
         self.last = nn.Linear(128,1)
     
     def forward(self, x):
-        x = F.relu(self.a1)
-        x = F.relu(self.a2)
-        x = F.relu(self.a3)
-        x = F.max_pool2d(x, 2)
+        x = F.relu(self.a1(x))
+        x = F.relu(self.a2(x))
+        x = F.relu(self.a3(x))
 
-        x = F.relu(self.b1)
-        x = F.relu(self.b2)
-        x = F.relu(self.b3) 
-        x = F.max_pool2d(x)
+        x = F.relu(self.b1(x))
+        x = F.relu(self.b2(x))
+        x = F.relu(self.b3(x)) 
 
-        x = F.relu(self.c1)
-        x = F.relu(self.c2)
-        x = F.relu(self.c3)
-        x = F.max_pool2d(x)
+        x = F.relu(self.c1(x))
+        x = F.relu(self.c2(x))
+        x = F.relu(self.c3(x))
 
-        x = F.relu(self.d1)
-        x = F.relu(self.d2)
-        x = F.relu(self.d3)
+        x = F.relu(self.d1(x))
+        x = F.relu(self.d2(x))
+        x = F.relu(self.d3(x))
 
         x = x.view(-1, 128)
         x = self.last(x)
 
-        return F.log_softmax(x)
+        return F.tanh(x)
 
-chess_dataset = ChessValueDataset()
-train_loader = torch.utils.data.DataLoader(chess_dataset, batch_size = 16)
-model = Net()
-optimizer = torch.optim.Adam(model.parameters())
+if __name__ == "__main__":
 
-device = 'cpu'
+    chess_dataset = ChessValueDataset()
+    # X, y = ChessValueDataset()
+    train_loader = torch.utils.data.DataLoader(chess_dataset, batch_size=256, shuffle=True)
+    model = Net()
+    optimizer = torch.optim.Adam(model.parameters())
+    floss = torch.nn.MSELoss()
+    num_epochs = 100
 
-model.train()
-for batch_idx, (data, target) in enumerate(train_loader):
-    print(data)
-    data = data.unsqueeze_(-1)
-    data, target = data.to(device), target.to(device)
-    optimizer.zero_grad()
-    output = model(data)
-    loss = F.nll_loss(output, target)
-    loss.backward()
-    optimizer.step()
+    device = 'cpu' #Train on cpu b/c not rich boy and don't have access to gpu 
+
+    model.train()
+
+    for epoch in range(num_epochs):
+        print('current epoch %d' % epoch)
+        all_loss = 0
+        num_loss = 0
+        for data, target in train_loader:
+            # print(local_batch,  local_labels)
+            # target = target.unsqueeze(-1)
+            data, target = data.to(device), target.to(device)
+            data = data.float()
+            target = target.float()
+            optimizer.zero_grad()
+            output = model(data)
+            loss = floss(output, target)
+            loss.backward()
+            optimizer.step()
+
+            all_loss += loss.item()
+            num_loss += 1
+
+        print("%3d: %f" % (epoch, all_loss/num_loss))
+        torch.save(model.state_dict(), "nets/value.pth")
